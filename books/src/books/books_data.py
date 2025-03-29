1cf7e9ff9c00097eb8751a3bc8ebbc8834e57451
Gustavo Castro <work@gvcastro.com>
2025-03-29 00:54:37 -0300

    Improved file structure and new routes implementation

--- books/src/books/books_data.py
+++ books/src/books/books_data.py
@@ -1,15 +1,3 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-
-app = FastAPI()
-
-"""
-/books GET
-/books POST
-/book/{book_id} PATCH
-/book/{book_id} DELETE
-"""
-
 books = [
     {
         "id": 1,
@@ -57,41 +45,3 @@ books = [
         "language": "English",
     },
 ]
-
-class Book(BaseModel):
-    id: int
-    title: str
-    author: str
-    publisher: str
-    published_data: str
-    page_count: int
-    language: str
-
-
-"""
-Get all books
-"""
-@app.get("/books", response_model=Book)
-async def get_all_books() -> list:
-    return books
- 
-"""
-Create new books
-"""
-@app.post("/books")
-async def create_books() -> dict:
-    pass
- 
-"""
-Update book resource
-"""
-@app.patch("/books/{book_id}")
-async def get_book(book_id: int) -> dict:
-    pass
-
-"""
-Delete book resource
-"""
-@app.patch("/books/{book_id}")
-async def delete_book(book_id: int) -> dict:
-    pass
