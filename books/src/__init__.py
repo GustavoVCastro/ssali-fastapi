17cc799ca1ac529a258a173ad1b6650e27b45ec5
Gustavo Castro <work@gvcastro.com>
2025-03-30 02:02:23 -0300

    Implemented connection with PostgreSQL

--- books/src/__init__.py
+++ books/src/__init__.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
 from src.books.routes import book_router
+from src.db.main import init_db
+
+
+@asynccontextmanager
+async def life_span(app: FastAPI):
+    await init_db()
+    print(f"server is starting...")
+    yield
+    print(f"server has been stopped")
+
 
 version = "v1"
 
 app = FastAPI(
     title="Books",
     description="A REST API for a book review web service",
-    version=version
+    version=version,
+    lifespan=life_span,
 )
 
-app.include_router(book_router, prefix="/api/{version}/books", tags=['books'])
+app.include_router(book_router, prefix="/api/{version}/books", tags=["books"])
